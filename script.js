af5adb2c97062fb2b282e797e40301c7285cb67e
mrrahulksuthan <86397472+mrrahulksuthan@users.noreply.github.com>
2023-06-10 12:41:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	 let hex = shortHex.replace("#", "");
+  if (hex.length === 3) {
+    hex = hex
+      .split("")
+      .map((char) => char + char)
+      .join("");
+  }
+  const fullHex = "#" + hex;
+  return fullHex;
 };
 
 // Do not change the code below.
